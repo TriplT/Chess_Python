67be34b946fba80d2278c3fafd879221b83cae98
TriplT <rekreut.mot@gmail.com>
2023-12-12 12:22:13 +0100

    nice

--- data/classes/player.py
+++
@@ -1,9 +0,0 @@
-from Pycharm_Projects.Chess_Test.data.classes.board import *
-
-
-class Player:
-
-    def __init__(self, color):
-        self.color = color
-        self.left_castling = True
-        self.right_castling = True
